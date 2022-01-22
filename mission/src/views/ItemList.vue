e5eb7bc814754cb531c81034ab82b34523ab1272
MiJung Lee <mijung@iimijeong-ui-MacBook-Air.local>
2022-01-23 03:54:14 +0900

    Header Nav 컴포넌트 추가

--- mission/src/views/ItemList.vue
+++ mission/src/views/ItemList.vue
@@ -1,10 +1,17 @@
 <template>
   <div id="item-list-page">
+    <Header />
+    <Nav />
+    <Item />
   </div>
 
 </template>
 
 <script>
+import Header from '../components/Header.vue';
+import Nav from '../components/Nav.vue';
+import Item from '../components/ItemList/Item.vue';
+
 export default {
   name: 'ItemListPage',
   data() {
@@ -12,11 +19,13 @@ export default {
     };
   },
   components: {
+    Header,
+    Nav,
+    Item,
   },
 };
 
 </script>
 
 <style>
-
 </style>
